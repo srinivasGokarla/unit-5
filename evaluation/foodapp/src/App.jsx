1dc4fe9fdb4cec0f678112a26950247ba444ff39
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-03 15:37:45 +0530

    3rd

--- evaluation/foodapp/src/App.jsx
+++ evaluation/foodapp/src/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
     <h1>Food App</h1>
+    <div className="big">
+    <div  className="left"></div>
+    <div className="right">
+
+    </div>
+    <div  className="bottom"></div>
+    </div>
     </div>
   );
 }
