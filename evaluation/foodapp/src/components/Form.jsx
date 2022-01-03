f72baa202ece7a347b6ac8f96fe3d5d8e959ea94
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-03 15:17:50 +0530

    2nd

---
+++ evaluation/foodapp/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import {useState, useRef} from "react"
+export const Form = () => {
+    const[form,setform] = useState();
+    const ref = useRef(null)
+    
+
+    return (
+        <form>
+            <input />
+        </form>
+    )
+}
