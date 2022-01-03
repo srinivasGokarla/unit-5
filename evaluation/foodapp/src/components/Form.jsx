9b728b5178bf802184935bb038686644b335dc21
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-03 16:24:54 +0530

    5th

--- evaluation/foodapp/src/components/Form.jsx
+++ evaluation/foodapp/src/components/Form.jsx
@@ -1,36 +1,47 @@
-import {useState,useRef} from "react"
+import {useState,useRef} from "react";
+import {useEffect} from "react/cjs/react.development"
 
 export const Form = () => {
-    const[form, setForm] = useState()
+    const[text, setText] = useState("")
+    const[test, setTest] = useState([])
     const ref = useRef(null)
+    useEffect(() => {
+
+    })
+    const addRecipe = () => {
+        
+    }
 
     return (
         <form>
         
           <lable>Title</lable>
           <br></br>
-          <input />
+          <input type="text" name="title" placeholder="Enter Recipe Name" />
           <br></br>
           <br></br>
 
           <lable>Ingrediants</lable>
           <br></br>
-          <input />
+          <input type="text" name="ingrediants" placeholder="Enter the Ingrediants" />
           <br></br>
           <br></br>
           <lable>Time to Cook</lable>
           <br></br>
-          <input />
+          <input type="number" name="Time to cook" placeholder="Enter the Time" />
           <br></br>
           <br></br>
           <lable>Image</lable>
           <br></br>
-          <input />
+          <input type="text" name="image" placeholder="Enter image url" />
           <br></br>
           <br></br>
           <lable>Instructions</lable>
           <br></br>
-          <input />
+          <input type="text" name="Instructions" placeholder="Enter the Instructions" />
+          <br></br>
+          <br></br>
+          <button>Submit</button>
         </form>
     )
 }
