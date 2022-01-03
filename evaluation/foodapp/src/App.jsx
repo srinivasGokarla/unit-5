c4053f92b768690d28a9cbe6098d59d4bb21f21e
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-03 17:07:14 +0530

    7th

--- evaluation/foodapp/src/App.jsx
+++ evaluation/foodapp/src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Form} from "./components/Form"
 
+
 function App() {
   return (
     <div className="App">
@@ -13,7 +14,7 @@ function App() {
       <Form />
     </div>
     <div className="right">
-
+  
     </div>
     <div  className="bottom"></div>
     </div>
--- evaluation/foodapp/src/components/Form.jsx
+++ evaluation/foodapp/src/components/Form.jsx
@@ -1,10 +1,10 @@
-import {useState,useRef} from "react";
+import {useState} from "react";
 import {useEffect} from "react/cjs/react.development"
 
 export const Form = () => {
     const[text, setText] = useState("")
     const[test, setTest] = useState([])
-    const ref = useRef(null)
+  
     useEffect(() => {
 
     })
@@ -39,28 +39,28 @@ export const Form = () => {
         
           <lable>Title</lable>
           <br></br>
-          <input value={text} onChange={(e) => setText(e.target.value)} type="text" name="title" placeholder="Enter Recipe Name" />
+          <input  onChange={(e) => setText(e.target.value)} type="text" name="title" placeholder="Enter Recipe Name" />
           <br></br>
           <br></br>
 
           <lable>Ingrediants</lable>
           <br></br>
-          <input value={text} onChange={(e) => setText(e.target.value)} type="text" name="ingrediants" placeholder="Enter the Ingrediants" />
+          <input onChange={(e) => setText(e.target.value)} type="text" name="ingrediants" placeholder="Enter the Ingrediants" />
           <br></br>
           <br></br>
           <lable>Time to Cook</lable>
           <br></br>
-          <input value={Number} onChange={(e) => setText(e.target.value)} type="number" name="timetocook" placeholder="Enter the Time" />
+          <input  onChange={(e) => setText(e.target.value)} type="number" name="timetocook" placeholder="Enter the Time" />
           <br></br>
           <br></br>
           <lable>Image</lable>
           <br></br>
-          <input value={text} onChange={(e) => setText(e.target.value)} type="text" name="image" placeholder="Enter image url" />
+          <input  onChange={(e) => setText(e.target.value)} type="text" name="image" placeholder="Enter image url" />
           <br></br>
           <br></br>
           <lable>Instructions</lable>
           <br></br>
-          <input value={text} onChange={(e) => setText(e.target.value)} type="text" name="instructions" placeholder="Enter the Instructions" />
+          <input  onChange={(e) => setText(e.target.value)} type="text" name="instructions" placeholder="Enter the Instructions" />
           <br></br>
           <br></br>
           <button onClick={ addRecipe }>Submit</button>
