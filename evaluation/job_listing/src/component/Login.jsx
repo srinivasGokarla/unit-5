5d84745dd14d0240ed6891b1ae490771e2e1abf6
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-17 15:14:14 +0530

    2nd

---
+++ evaluation/job_listing/src/component/Login.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div style= {{
+            display: 'flex',
+            border: '1px solid red',
+            height: '10%',
+            color: 'white'
+        }} >
+<div>Home</div>
+<div>Admin</div>
+<div>Login</div>
+<div>Applyjob</div>
+        </div>
+    )
+}
