72c2d75f3f5c222db960de88bbb16785a76d1bb1
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-17 16:24:18 +0530

    3rd

--- evaluation/job_listing/src/App.jsx
+++ evaluation/job_listing/src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import{Navbar} from "./component/Navbar"
 
 function App() {
   return (
     <div className="App">
-     
+      < Navbar />
+     <h1>Job Application</h1>
     </div>
   );
 }
---
+++ evaluation/job_listing/src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+   
+    return (
+        <div className="nav" >
+<div>Home</div>
+<div>Admin</div>
+<div>Login</div>
+<div>Applyjob</div>
+        </div>
+    )
+}
+export {Navbar}
